6a5a1cb2ab67064e381131892e9db859814d769f
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-16 17:40:19 +0200

    added landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from "./components/Navbar/Navbar.tsx";
+import LandingPage from "./components/LandingPage/LandingPage.tsx";
 
 function App() {
 
   return (
     <>
         <Navbar />
+        <div className="container">
+            <LandingPage />
+        </div>
     </>
   )
 }
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      LandingPage
+    </div>
+  );
+}
+
+export default LandingPage;
