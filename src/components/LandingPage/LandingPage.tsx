1dadb6387337814dd4e1b980b4b0e3814948b43d
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-19 21:03:57 +0200

    started with the LandingPage

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,24 @@
 import "./LandingPage.css";
 
 function LandingPage() {
-  return (
-    <div className="landing-page">
-        <h1 className={""}>Jonas</h1>
-    </div>
-  );
+    return (
+        <div className="landing-page">
+
+            <div className="split">
+                <div className="left">
+                    <img src={"src/assets/portrait.png"} alt="" className="portrait-image" />
+                </div>
+                <div className="right">
+                    <h1 className="h1">Jonas Berger</h1>
+                    <p>
+                        Hi, im jonas and i have a really <del>dumb</del> <ins>genious</ins> website!!
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default LandingPage;
