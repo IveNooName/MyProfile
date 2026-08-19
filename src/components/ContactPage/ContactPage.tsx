c019acd17402dac875901d5645a9b50db647ff49
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-19 22:12:36 +0200

    added address

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -3,7 +3,14 @@ import "./ContactPage.css";
 function ContactPage() {
   return (
     <div className="contact-page">
-      ContactPage
+      <h1>Contact</h1>
+
+        <address>
+            Erstellt von <strong>Jonas Berger</strong> <br/>
+            igkf-Strasse 88 <br/>
+            3088 Skid-Dorf <br/>
+            SWITZERLAND
+        </address>
     </div>
   );
 }
