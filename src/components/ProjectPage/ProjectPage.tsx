cb1d46ca0da62591ae85e102da94a7cc9e1471b7
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-24 09:26:12 +0200

    added Projectpage

---
+++ src/components/ProjectPage/ProjectPage.tsx
@@ -0,0 +1,13 @@
+import "./ProjectPage.css";
+
+interface ProjectPageProps {}
+
+function ProjectPage(props: ProjectPageProps) {
+  return (
+    <div className="project-page">
+      ProjectPage
+    </div>
+  );
+}
+
+export default ProjectPage;
