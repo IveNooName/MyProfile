516c00f036214a7b8757180b324b092023f4f0c9
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-24 09:33:27 +0200

    added projectCard

---
+++ src/components/ProjectPage/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,15 @@
+import "./ProjectCard.css";
+
+interface ProjectCardProps {
+
+}
+
+function ProjectCard(props: ProjectCardProps) {
+  return (
+    <div className="project-card">
+      ProjectCard works!
+    </div>
+  );
+}
+
+export default ProjectCard;
