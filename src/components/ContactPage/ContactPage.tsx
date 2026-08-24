7b71b87edfe63173cf4720d717dab04ffcdc29fa
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-24 08:25:12 +0200

    added contactPage

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -2,16 +2,23 @@ import "./ContactPage.css";
 
 function ContactPage() {
   return (
-    <div className="contact-page">
-      <h1>Contact</h1>
+      <div>
+          <div className="contact-page">
+              <h1>Contact</h1>
 
-        <address>
-            Erstellt von <strong>Jonas Berger</strong> <br/>
-            igkf-Strasse 88 <br/>
-            3088 Skid-Dorf <br/>
-            SWITZERLAND
-        </address>
-    </div>
+              <address>
+                  Created by <strong>Jonas Berger</strong> <br/>
+                  igkf-Strasse 88 <br/>
+                  3088 Skid-Dorf <br/>
+                  SWITZERLAND
+              </address>
+          </div>
+          <footer className="border-top py-3">
+              <span className={"h4"}>Jonas Berger</span>
+              <br/>
+              <span className={"small text-black-50"}>© Jöggu, 2026</span>
+          </footer>
+      </div>
   );
 }
 
