9bc9b88e7a809ba7571de2df58a19311ecd6bc15
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-26 22:11:02 +0200

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="footer">
         <footer className="border-top py-3">
+            <hr/>
             <span className={"h4"}>Jonas Berger</span>
             <br/>
             <span className={"small text-black-50"}>© Jöggu, 2026</span>
